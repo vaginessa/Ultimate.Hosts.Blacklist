88532b00e49ec855f82ea743824087816ac2d29f
funilrys <contact@funilrys.com>
2017-09-17 10:04:32 +0200

    Clean reviewed logs [funceble skip]
    Former-commit-id: 51cad9e4a8d58053d3293b00b3de746a32ea210e

--- .dev-tools/_funceble/output/logs/noReferer/.ml
+++
@@ -1,4 +0,0 @@
-====================================================================================================
-No referer found for: ml domains
-Tested domain: 962583-deutschland-nutzung-mitteilung-konto_identity.sicher-sicherheitsabfrage.ml
-====================================================================================================
